4416c73ad34c6605cfdba38ef76953fd28f23428
Dominik Czerwiński <czerwin94@gmail.com>
2020-01-23 10:23:11 +0100

    Update Robot.ino

--- Kod - robot/Robot.ino
+++ Kod - robot/Robot.ino
@@ -3,7 +3,7 @@
 #ifndef APSSID
 #define APSSID "Robot"
 #endif
-#ifndef PORT
+#ifndef PORT 
 #define PORT 12345
 #endif
 
@@ -20,6 +20,7 @@ int multi_= 5;
 int time_= 25;
 int led=2;    
 bool fDrive = false;
+
 
 void drive(int I1, int I2, int I3, int I4, int multi, int t){
   digitalWrite(IN1,I1);
@@ -54,9 +55,9 @@ void turn(int I1, int I2, int I3, int I4, int t){
   digitalWrite(IN2,I2);
   digitalWrite(IN3,I3);
   digitalWrite(IN4,I4);
-  analogWrite(ENA,450);
-  analogWrite(ENB,450);
-  delay(400);
+  analogWrite(ENA,400);
+  analogWrite(ENB,400);
+  delay(700);
   digitalWrite(IN1,LOW);
   digitalWrite(IN2,LOW);
   digitalWrite(IN3,LOW);
@@ -65,15 +66,14 @@ void turn(int I1, int I2, int I3, int I4, int t){
   analogWrite(ENB,0);
   delay(500);
 }
-void freeDrive(int I1, int I2, int I3, int I4){
-  analogWrite(ENA,1024);
-  analogWrite(ENB,1024);
+void freeDrive(int I1, int I2, int I3, int I4, int spd){
+  analogWrite(ENA,spd);
+  analogWrite(ENB,spd);
   digitalWrite(IN1,I1);
   digitalWrite(IN2,I2);
   digitalWrite(IN3,I3);
   digitalWrite(IN4,I4);
 }
-
 
 void setup(){
   pinMode(IN1,OUTPUT); 
@@ -82,7 +82,7 @@ void setup(){
   pinMode(IN4,OUTPUT); 
   pinMode(ENA,OUTPUT); 
   pinMode(ENB,OUTPUT); 
-  WiFi.mode(WIFI_STA);
+  WiFi.mode(WIFI_STA); 
   WiFi.disconnect(); 
   delay(100);
   
@@ -90,14 +90,14 @@ void setup(){
   Serial.println();
   Serial.print("Konfiguracja AP...");
   
-  IPAddress ip(192,168,1,200); 
+  IPAddress ip(192,168,1,200);
   IPAddress gateway(192,168,1,254);
   IPAddress subnet(255,255,255,0);
-  WiFi.softAPConfig(ip, gateway, subnet); 
-  WiFi.softAP(ssid,pass,1,false,1); 
+  WiFi.softAPConfig(ip, gateway, subnet);
+  WiFi.softAP(ssid,pass,1,false,2);
   IPAddress myIP = WiFi.softAPIP(); 
-  Serial.print("AP IP: ");  
-  Serial.println(myIP);             
+  Serial.print("AP IP: "); 
+  Serial.println(myIP);          
   
   server.begin(); 
 
@@ -111,7 +111,7 @@ void setup(){
 
 
 void loop(){
-  WiFiClient client = server.available(); 
+  WiFiClient client = server.available();
   
   if (client){
     if(client.connected()){
@@ -119,48 +119,52 @@ void loop(){
     }
     while(client.connected()){
       while(client.available()>0){
+        // read data from the connected client
+        //Serial.write(client.read());
+        int readed = client.read();
+        /*
         String line = client.readStringUntil('\n');
         Serial.println(line);
         char cmd[line.length()+1];
         line.toCharArray(cmd, line.length()+1);
         for(int i = 0; i < line.length()+1; i ++){
           switch(cmd[i]){
+          */
+          switch(readed){
             case 'u':
               fDrive = false;
-              drive(HIGH, LOW, HIGH, LOW, 5, 10);
+              drive(HIGH, LOW, HIGH, LOW, 5, 1024);
               break;
             case 'd':
               fDrive = false;
-              drive(LOW, HIGH, LOW, HIGH, 5, 10);
+              drive(LOW, HIGH, LOW, HIGH, 5, 1024);
               break;
             case 'l':
               fDrive = false;
-              turn(LOW, HIGH, HIGH, LOW, 950);
+              turn(LOW, HIGH, HIGH, LOW, 450);
               break;
             case 'r':
               fDrive = false;
-              turn(HIGH, LOW, LOW, HIGH, 950);
+              turn(HIGH, LOW, LOW, HIGH, 450);
               break;
             case 'z':
               fDrive = true;
-              freeDrive(HIGH, LOW, HIGH, LOW);
+              freeDrive(HIGH, LOW, HIGH, LOW, 1024);
               break;
             case 'x':
               fDrive = true;
-              freeDrive(LOW, HIGH, LOW, HIGH);
+              freeDrive(LOW, HIGH, LOW, HIGH, 1024);
               break;
             case 'c':
               fDrive = true;
-              freeDrive(LOW, HIGH, HIGH, LOW);
+              freeDrive(LOW, HIGH, HIGH, LOW, 1024);
               break;
             case 'v':
               fDrive = true;
-              freeDrive(HIGH, LOW, LOW, HIGH);
+              freeDrive(HIGH, LOW, LOW, HIGH, 1024);
               break;
             case 's':
               fDrive = false;
-              freeDrive(LOW, LOW, LOW, LOW);
-              drive(LOW,LOW,LOW,LOW,1,0);
               digitalWrite(IN1,LOW); 
               digitalWrite(IN2,LOW); 
               digitalWrite(IN3,LOW); 
@@ -169,10 +173,15 @@ void loop(){
               analogWrite(ENB,0);
               break;              
             default:
-              if(!fDrive) drive(LOW,LOW,LOW,LOW,1,0);
+              if(!fDrive){
+                digitalWrite(IN1,LOW); 
+              digitalWrite(IN2,LOW); 
+              digitalWrite(IN3,LOW);
+              digitalWrite(IN4,LOW); 
+              }
               break;
           }
-        }
+        //}
       //Send Data to connected client
       while(Serial.available()>0){
         client.write(Serial.read());
